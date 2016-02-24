39ff882bdf7e9fab27f66f4c62d3a4c373d72d52
Andre Hoffmann <dev@effzehn.de>
2016-02-24 11:13:49 +0100

    Initial Commit

---
+++ EZTamperDetection/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  EZTamperDetection
+//
+//  Created by Developer on 24.02.16.
+//  Copyright © 2016 Andre Hoffmann. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ EZTamperDetection/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  EZTamperDetection
+//
+//  Created by Developer on 24.02.16.
+//  Copyright © 2016 Andre Hoffmann. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
